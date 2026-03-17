976dba1d976d26c5b75132e1963b0f27c9095fb0
viktor-bd <viktordorph@hotmail.com>
2026-03-17 12:25:25 +0100

    Change CalculatorService syntax

--- src/Api/Services/CalculatorService.cs
+++ src/Api/Services/CalculatorService.cs
@@ -7,7 +7,7 @@ public class CalculatorService : ICalculatorService
     public double Multiply(double a, double b) => a * b;
     public double Divide(double a, double b)
     {
-        if (b == 0) throw new ArgumentException("Division by zero is not allowed.");
+        if (b == 0) throw new ArgumentException("Division by zero is not allowed!");
         return a / b;
     }
 }
